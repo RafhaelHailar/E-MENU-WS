78a5e12b21916eb64a39b6a38f35050503bfa6dc
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-16 16:36:50 +0800

    [Feat] add cart event.

--- events/cart.event.ts
+++ events/cart.event.ts
@@ -2,13 +2,17 @@ import { Socket } from "socket.io";
 import getOrCacheOrders from "../utils/getOrCacheOrders";
 import redis from "../lib/redis";
 import io from "../lib/socket.io";
+import getOrCacheTableStatus from "../utils/getOrCacheTableStatus";
 
 
 export default function(socket: Socket) {
     const { userSession, tableSession } = socket.handshake.query as  { userSession: string, tableSession: string }; 
 
-    socket.on("add cart", (data) => {
+    socket.on("add cart", async (data) => {
         const productId = data.productId;
+        const tableStatus = JSON.parse(await getOrCacheTableStatus(tableSession));
+
+        if (tableStatus.status !== 200) return socket.emit("error add cart", tableStatus);
 
         const key = `${tableSession}-cart:${productId}`;
 
