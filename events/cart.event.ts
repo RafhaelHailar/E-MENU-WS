301f97e32f92138fdb99d8d929d5761b6c9885f9
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-16 17:16:25 +0800

    [Feat] update cart item quantity.

--- events/cart.event.ts
+++ events/cart.event.ts
@@ -3,9 +3,10 @@ import getOrCacheOrders from "../utils/getOrCacheOrders";
 import redis from "../lib/redis";
 import io from "../lib/socket.io";
 import getOrCacheTableStatus from "../utils/getOrCacheTableStatus";
+import sendCartItemsUpdate from "../utils/sendCartItemsUpdate";
 
 
-export default function(socket: Socket) {
+export default async function(socket: Socket) {
     const { userSession, tableSession } = socket.handshake.query as  { userSession: string, tableSession: string }; 
 
     socket.on("add cart", async (data) => {
@@ -16,6 +17,26 @@ export default function(socket: Socket) {
 
         const key = `${tableSession}-cart:${productId}`;
 
-        console.log(key);
+        try {
+            const cartItemQuantity = await redis.get(key);
+    
+            if (!cartItemQuantity) {
+                await redis.set(key, 1);
+            } else {
+                await redis.set(key, Number(cartItemQuantity) + 1);
+            }
+
+            await sendCartItemsUpdate(socket, tableSession);
+        } catch (e) {
+            console.log(e);
+        }
+        
     });
+
+    // send cart items data
+    try {
+        await sendCartItemsUpdate(socket, tableSession);
+    } catch (e) {
+        console.log(e);
+    }
 }
