3c37e7f45cf722590378f755e2512fb6d976422a
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-16 15:25:40 +0800

    [Update] added important cookie.

--- api/updateOrderStatus.ts
+++ api/updateOrderStatus.ts
@@ -1,19 +1,20 @@
 
 async function updateOrderStatus(userSession, orderNo, status) {
-
+    
     try {
-        const request = await fetch(`${process.env.API_SERVER_BASE_URL}/`, {
+        const request = await fetch(`${process.env.API_SERVER_BASE_URL}/order/status`, {
             method: "POST",
             credentials: "include",
             headers: {
                 "Content-Type": "application/json",
-                "Cookie": `_table_session=${userSession}`
+                "Cookie": `_user_session=${userSession}`
             },
             body: JSON.stringify({orderNo,status})
         });
     
         const data = await request.json();
     
+        console.log(data.message);
         return { status: request.status, data};
     } catch (error) {
         return { error };
