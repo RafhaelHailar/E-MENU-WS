70c9095d0e582f427235fa20dce25fbfc11b1773
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-16 14:00:09 +0800

    [Feat] get customer latest order

--- api/getMyLatestOrder.ts
+++ api/getMyLatestOrder.ts
@@ -1,15 +1,20 @@
 
 async function getMyLatestOrder(tableSession) {
-    const request = await fetch(`${process.env.API_SERVER_BASE_URL}/my_latest_order`, {
-        credentials: "include",
-        headers: {
-            "Cookie": `_table_session=${tableSession}`
-        },
-    });
 
-    const latestOrder = await request.json();
-
-    return latestOrder;
+    try {
+        const request = await fetch(`${process.env.API_SERVER_BASE_URL}/my_latest_order`, {
+            credentials: "include",
+            headers: {
+                "Cookie": `_table_session=${tableSession}`
+            },
+        });
+    
+        const latestOrder = await request.json();
+    
+        return { status: request.status, data: latestOrder};
+    } catch (error) {
+        return { error };
+    }
 }
 
 export default getMyLatestOrder;
