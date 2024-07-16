e6ac3f37677560bae96abb4601d453f0e9a50247
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-17 05:35:06 +0800

    [Feat] set and get functions.

--- lib/redis.ts
+++ lib/redis.ts
@@ -3,3 +3,13 @@ import { Redis } from "ioredis";
 const redis = new Redis(`rediss://default:${process.env.REDIS_DB_PASSWORD}@${process.env.REDIS_DB_HOST}:${process.env.REDIS_DB_PORT}`);
 
 export default redis;
+
+export async function redisGet(key: string) {
+    try {
+        const data = await redis.get(key);
+
+        return { data: JSON.parse(data) };
+    } catch (error) {
+        return { error };
+    }
+}
