2e2d89427bcd7e97f066c660a130e1410bc8a0f1
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-18 13:54:23 +0800

    [Refactor] use new send table queues.

--- events/session.event.ts
+++ events/session.event.ts
@@ -1,18 +1,15 @@
 import { Socket } from "socket.io";
 import getAllTableSessions from "../utils/getAllTableSessions";
+import sendTableQueues from "../utils/sendTableQueues";
 
 export default async function(socket: Socket) {
     const { userSession } = socket.handshake.query as  { userSession: string }; 
 
     socket.on("get table queues", async () => {
-        console.log("hello from client")
-        const tableSessions = await getAllTableSessions();
-        console.log(tableSessions);
-        socket.emit("table queues sent", tableSessions);
+        await sendTableQueues();
     });
 
     if (userSession) {
-        const tableSessions = await getAllTableSessions();
-        socket.emit("table queues sent", {data: tableSessions});
+        await sendTableQueues();
     }
 }
