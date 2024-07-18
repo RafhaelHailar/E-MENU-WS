ae4efd4d0f3dc6284d091a4f1b9178c826596d3e
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-18 10:34:13 +0800

    [Feat] get or cache table sessions.

---
+++ utils/getOrCacheTableSessions.ts
@@ -0,0 +1,15 @@
+import { redisGet, redisSet } from "../lib/redis";
+import getTableSessions from "api/getTableSessions";
+
+async function getOrCacheTableSessions(userSession: string) {
+    const key = "table_session";
+
+    const sessions = await redisGet(key);
+    if (sessions.error || sessions.data != null) return sessions;
+    
+    const sessionsData = await getTableSessions(userSession);
+    redisSet(key, sessionsData.data);
+    return { ...sessionsData };
+}
+
+export default getOrCacheTableSessions;
