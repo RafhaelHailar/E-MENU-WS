f90a760d6e09d62bb7574a11a45d180c258b84d5
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-27 18:26:40 +0800

    [Feat] update table session.

--- events/session.event.ts
+++ events/session.event.ts
@@ -1,12 +1,19 @@
 import { Socket } from "socket.io";
-import getAllTableSessions from "../utils/getAllTableSessions";
 import sendTableQueues from "../utils/sendTableQueues";
+import { redisGet } from "../lib/redis";
 
 export default async function(socket: Socket) {
     const { userSession } = socket.handshake.query as  { userSession: string }; 
 
     socket.on("get table queues", async () => {
         await sendTableQueues();
+    });
+
+    socket.on("table session updated", async ({session,status}) => {  
+        const socketId = await redisGet(`customer-session-${session}`);
+        const socket = io.sockets.sockets.get(socketId);
+        
+        socket.emit("my table sesson update", { status });    
     });
 
     if (userSession) {
