6e9b27768174109039f894f57848da4e24723dac
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-27 18:27:23 +0800

    [Feat] get a particular table session.

--- utils/sendTableQueues.ts
+++ utils/sendTableQueues.ts
@@ -1,8 +1,8 @@
-import getAllTableSessions from "./getAllTableSessions";
 import io from "../lib/socket.io";
+import getTableSessions from "../api/getTableSessions";
 
 async function sendTableQueues() {
-    const tableSessions = await getAllTableSessions();
+    const tableSessions = await getTableSessions();
     io.emit("table queues sent", {data: tableSessions});
 }
 
